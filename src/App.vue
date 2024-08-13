15da306245858c515f0b16247440094c13b30cc6
zhangtao <1792670518@qq.com>
2024-08-13 16:39:24 +0800

    hello

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ const watermarkProps = computed<WatermarkProps>(() => {
     <AppProvider>
       <!--   路由页面    -->
       <RouterView />
-      <NButton>按钮</NButton>
+      <NButton>按钮111</NButton>
       <!--   水印   -->
       <NWatermark v-bind="watermarkProps" />
     </AppProvider>
